5ca60a8ecaa6f5b681065c5291a918fa43e4cdb6
helican <40611317+helican@users.noreply.github.com>
2018-07-25 02:04:18 -0400

    Update prime100.sh

--- prime100.sh
+++ prime100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Print Primenumber between 1 to 100
 prime_1=0
-echo " Primenumber between 1 to 100 is:"
+echo " Primenumber between 1 to $n is:"
 echo "1"  
 echo "2"
 for((i=3;i<=100;))
