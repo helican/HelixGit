3c9d3a2ba52b06ddad3bd1d162cc90c99f4f30ac
helican <helicans@yahoo.com>
2018-09-10 15:48:11 -0700

    prime file modified

--- prime100.sh
+++ prime100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Print Primenumber between 1 to 100
 prime_1=0
-echo " Primenumber between 1 to 100 is:"
+echo " Prime numbers starting from 1 to 100 are:"
 echo "1"  
 echo "2"
 for((i=3;i<=100;))
